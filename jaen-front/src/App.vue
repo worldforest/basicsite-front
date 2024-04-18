20e71ef819aa345329fe5fb86989649a2f8b1fa9
serim.jaen <serim.jaen@androidjava.com>
2024-04-18 18:17:44 +0900

    네이게이션바 드롭다운 안됨

--- jaen-front/src/App.vue
+++ jaen-front/src/App.vue
@@ -54,7 +54,6 @@ export default {
   }
 }
 </script>
-
 <style>
 #app {
   width: 100%;
--- jaen-front/src/components/layout/JaenHeader.vue
+++ jaen-front/src/components/layout/JaenHeader.vue
@@ -1,78 +1,102 @@
 <template>
-  <div class="navbar-container ">
-      <nav class="navbar navbar-expand-lg " data-overlay >
-          <a class="navbar-brand fade-page" @click="goToHome" >
-            <img src="@/assets/img/main-logo.png" alt="자앤" width="90" height="38">
-          </a>
-          <!-- 크기 줄었을 때 Navbar 처리 -->
-          <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
-            <img class="icon navbar-toggler-open" src="@/assets/img/icons/interface/menu.svg" alt="menu interface icon" data-inject-svg />
-            <img class="icon navbar-toggler-close" src="@/assets/img/icons/interface/cross.svg" alt="cross interface icon" data-inject-svg />
-          </button> -->
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown link
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </nav>
+  <nav class="navbar navbar-expand-lg " data-overlay >
+    <a class="navbar-brand fade-page" @click="goToHome" >
+      <img src="@/assets/img/main-logo.png" alt="자앤" width="90" height="38">
+    </a>
+    <div class="dropdown" @mouseenter="toggleDropdown(true)" @mouseleave="toggleDropdown(false)">
+      <a>교육과정</a>
+      <div class="dropdown-content" v-show="isDropdownOpen">
+        <a @click="goToAllCategories()">전체과정</a>
+        <template v-for="category in categories" :key="category.category_id">
+          <div @click="selectCategory(category.category_id)" class="category-item">
+            <a>{{ category.name }}</a>
+            <!-- 카테고리 옆에 subcategory.name을 표시합니다. -->
+            <a v-for="subcategory in filteredItems()" :key="subcategory.name">
+              {{ filteredItems }}
+              {{ subcategory }}
+            </a>
+          </div>
+        </template>
+      </div>
     </div>
+  </nav>
 </template>
 
 <script>
+
 export default {
   name: "JaenHeader",
-
   data() {
     return {
       isDropdownOpen: false,
-    };
+      categories:[],
+      selectedCategoryId: null,
+      subcategories:[]
+    }
   },
   methods: {
+    // 마우스 hover에 따라 dropdown
     toggleDropdown() {
       this.isDropdownOpen = !this.isDropdownOpen;
     },
     goToHome(){
       this.$router.push('/');
     },
-    goToHistory(){
-      this.$router.push('/history');
-    },
-    goToContact(){
-      this.$router.push('/contact');
-    },
-    goToSystem(){
-      this.$router.push('/system');
-    },
-    goToCategories(){
-      this.$router.push('/categories');
-    },
-    goToClassAll(){
-      this.$router.push('/class_all');
+    getCategory() {
+        // 대분류 카테고리 가져오는 비동기 함수
+        this.axios.get("/categories").then((response) => {
+            this.categories = response.data;
+        });
     },
     goToAllCategories(){
       this.$router.push('/categories');
+    },
+    // 대분류 선택하면 중분류 가져오는 함수에 전달
+    selectCategory(categoryId){
+      this.selectedCategory = categoryId;
+      this.getSubcategory(this.selectedCategory);
+    },
+    // 중분류 카테고리 가져오는 비동기 함수
+    getSubcategory(categoryId) {
+        this.axios.get(`/subcategory?categoryId=${categoryId}`).then((response) => {
+            this.subcategories = response.data;
+        }).catch((error) => {
+            console.error('Error fetching data:', error);
+        });
+    },
+    filteredItems(){
+      console.log(this.subcategories);
+      return this.subcategories.filter(subcategory => subcategory.category_id === this.selectedCategoryId);
     }
+  },
+  mounted(){
+      // 화면이 로드되자마자
+      this.getCategory();
   },
 };
 </script>
+<style>
+.dropdown{
+  position: relative;
+  display: inline-block;
+}
+.dropdown-content{
+  display: none;
+  position: absolute;
+  background-color: aliceblue;
+  min-width: 160px;
+  z-index: 1;
+}
+.dropdown:hover .dropdown-content{
+  display: block;
+}
+.dropdown-content a{
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+.dropdown-content a:hover{
+  background-color: #f1f1f1;
+}
+</style>
